c6a0f6463ccdf825dd9bfef191e1f6a1ad0bb1bf
sobayashi <sophieobayashi@gmail.com>
2018-04-24 13:48:52 -0700

    added base html content

---
+++ assets/javascript/game.js
@@ -0,0 +1,23 @@
+
+/*
+if (the player presses any key) {
+    generate random word
+}
+
+if (the player types a letter) {
+    if (the letter is present in the word) {
+        update progress of word
+    }
+    else {
+        display letter at bottom of screen
+        add trump body part
+    }
+} else {
+    do nothing?
+}
+
+for (var i = 0; i < 9; i++) {
+    guesses
+}
+
+*/
